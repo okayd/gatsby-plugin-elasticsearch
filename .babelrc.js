1634b9a42b9ad8dbbd7800a6cc1a10318ece89b2
Hays Clark <hays@okayd.com>
2021-11-15 17:39:25 -0800

    fix(deps): moving packages included in gastby to devDependencies

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,3 @@
 module.exports = {
   presets: [[`babel-preset-gatsby-package`]],
-  plugins: [
-    [
-      `@babel/plugin-proposal-private-methods`,
-      {
-        loose: true,
-      },
-    ],
-    [
-      `@babel/plugin-proposal-class-properties`,
-      {
-        loose: true,
-      },
-    ],
-  ],
 };
